92ffde15664aaf635268ec7af662ff4a7d8dd829
Bilal Baheer <71363925+BilalBaheer@users.noreply.github.com>
2024-07-30 00:03:08 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from 'firebase/firestore';
-
-// Your web app's Firebase configuration
+import { getFirestore } from "firebase/firestore";
 
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 export { db };
